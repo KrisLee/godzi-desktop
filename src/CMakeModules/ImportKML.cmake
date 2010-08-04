aaf633a664805a1aa648dd116966c05fb7efcdf1
Cedric Pinson <cedric.pinson@plopbyte.net>
2010-08-04 10:07:39 +0200

    Fix compile issue, and add kml lib support to the project

---
+++ src/CMakeModules/ImportKML.cmake
@@ -0,0 +1,46 @@
+# Finds and imports sqlite3 library.
+
+create_imported_library(
+    KML_DOM
+    kml/dom.h kmldom SHARED
+    INCLUDE_SEARCH_PATH ${KML_DIR}/include ENV{KML_DIR}/include
+    LIBRARY_SEARCH_PATH ${KML_DIR}/lib ENV{KML_DIR}/lib
+)
+
+create_imported_library(
+    KML_BASE
+    kml/base/zip_file.h kmlbase SHARED
+    INCLUDE_SEARCH_PATH ${KML_DIR}/include ENV{KML_DIR}/include
+    LIBRARY_SEARCH_PATH ${KML_DIR}/lib ENV{KML_DIR}/lib
+)
+
+create_imported_library(
+    KML_ENGINE
+    kml/engine.h kmlengine SHARED
+    INCLUDE_SEARCH_PATH ${KML_DIR}/include ENV{KML_DIR}/include
+    LIBRARY_SEARCH_PATH ${KML_DIR}/lib ENV{KML_DIR}/lib
+)
+
+create_imported_library(
+    KML_XSD
+    kml/xsd/xst_parser.h kmlxsd SHARED
+    INCLUDE_SEARCH_PATH ${KML_DIR}/include ENV{KML_DIR}/include
+    LIBRARY_SEARCH_PATH ${KML_DIR}/lib ENV{KML_DIR}/lib
+)
+
+create_imported_library(
+    KML_CONVENIENCE
+    kml/convenience/convenience.h kmlconvenience SHARED
+    INCLUDE_SEARCH_PATH ${KML_DIR}/include ENV{KML_DIR}/include
+    LIBRARY_SEARCH_PATH ${KML_DIR}/lib ENV{KML_DIR}/lib
+)
+
+create_imported_library(
+    KML_REGIONATOR
+    kml/regionator/regionator.h kmlregionator SHARED
+    INCLUDE_SEARCH_PATH ${KML_DIR}/include ENV{KML_DIR}/include
+    LIBRARY_SEARCH_PATH ${KML_DIR}/lib ENV{KML_DIR}/lib
+)
+
+set(KML_ALL_LIBDEPENDENCIES
+    KML_DOM KML_BASE KML_ENGINE KML_XSD KML_CONVENIENCE KML_REGIONATOR)
