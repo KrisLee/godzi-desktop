857c9eed259e2245993b2feb4e334c37bc89c39b
Cedric Pinson <cedric.pinson@plopbyte.net>
2010-08-05 12:08:41 +0200

    Add basic files for placemark,feature

---
+++ src/SDK/src/Godzi/Features/Placemark.cpp
@@ -0,0 +1,39 @@
+/* --*-c++-*-- */
+/**
+ * Godzi
+ * Copyright 2010 Pelican Mapping
+ * http://pelicanmapping.com
+ * http://github.com/gwaldron/godzi
+ *
+ * Godzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include <Godzi/Features/Placemark>
+
+using namespace Godzi::Features;
+
+Placemark::Placemark() {}
+
+Placemark::Placemark(const Placemark& pm, const osg::CopyOp& cp):
+    Feature(pm, cp),
+    _coordinate(pm._coordinate)
+{
+}
+
+void Placemark::setCoordinates(double lat, double lon, double alt)
+{
+    _coordinate[0] = lat;
+    _coordinate[1] = lon;
+    _coordinate[2] = alt;
+}
